ceb7fe80084bcb2c9715186092b04b81582b9674
Dylan <rattledew17@gmail.com>
2024-09-28 14:09:37 -0700

    modified score zero placeholders

--- Interface/Gameplay/score.gd
+++ Interface/Gameplay/score.gd
@@ -17,25 +17,8 @@ func _on_combo_increment(is_increment: bool) -> void:
 func _on_score_increment() -> void:
 	current_score += 100 * combo
 	
-	# allows for the zero placeholders on the left
-	var zero_placeholder: String = ""
-	if current_score >= 10000000:
-		pass
-	elif current_score >= 1000000:
-		zero_placeholder = "0"
-	elif current_score >= 100000:
-		zero_placeholder = "00"
-	elif current_score >= 10000:
-		zero_placeholder = "000"
-	elif current_score >= 1000:
-		zero_placeholder = "0000"
-	elif current_score >= 100:
-		zero_placeholder = "00000"
-	elif current_score >= 10:
-		zero_placeholder = "000000"
-	
 	# if player reaches max score, the score will cap
 	if current_score >= 99999900:
 		current_score = 99999900
 
-	text = zero_placeholder + str(current_score)
+	text = str(current_score).pad_zeros(8)
