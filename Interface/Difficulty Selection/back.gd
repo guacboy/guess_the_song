1e85bde2371c0d514f7f8517cc14e19af66a1561
Dylan <rattledew17@gmail.com>
2024-10-08 14:55:25 -0700

    added back button
    added textures to difficulty selection

--- Interface/Difficulty Selection/back.gd
+++ Interface/Difficulty Selection/back.gd
@@ -1,5 +1,9 @@
-extends Control
+extends TextureButton
 
 func _input(event) -> void:
 	if Input.is_action_pressed("back"):
 		get_tree().change_scene_to_file("res://Interface/Song Selection/song_selection.tscn")
+		
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://Interface/Song Selection/song_selection.tscn")
